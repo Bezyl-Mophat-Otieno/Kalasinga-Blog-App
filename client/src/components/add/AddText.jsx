20faf842729f8396d9ef3212831ba02f309d9c7c
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-03-10 10:54:19 +0300

    removed draft.js

--- client/src/components/add/AddText.jsx
+++ client/src/components/add/AddText.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
